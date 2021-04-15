e087ad00cbac43e122996d4723ad48f96187eed9
edrichmkhasi <edrich@thedigitalacademy.co.za>
2021-04-15 11:03:24 +0200

    added dto to the project

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import {v1 as uuid} from 'uuid';
+import {CreateTaskDto} from '../tasks/dto/create-task.dto'
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,9 @@ export class TasksService {
       return this.tasks;
    }
 
-   createTask(title: string, description: string): Task {
+   createTask(createTaskDto: CreateTaskDto): Task {
+      const {title, description} = createTaskDto;
+      
       const task: Task = {
          id: uuid(),
          title,
